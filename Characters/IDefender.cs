78c9f3fe7519145a2d83c87de4939e2771ccb228
danielursuu <danielursuu@gmail.com>
2019-06-23 12:22:24 +0300

    implemented strategy pattern

---
+++ Characters/IDefender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_Game
+{
+    public interface IDefender
+    {
+        void Defend(int attackPoints);
+    }
+}
---
+++ Strategy/ISuperPower.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_Game
+{
+    public interface ISuperPower
+    {
+        SuperPower AttackWithSuperPower();
+    }
+}
