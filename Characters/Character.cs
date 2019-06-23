eb6f7d203289338ad4a91ad94d0f14e9455e2813
danielursuu <danielursuu@gmail.com>
2019-06-23 12:22:12 +0300

    implemented character and hero

---
+++ Characters/Character.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_Game.Characters
+{
+    public abstract class Character
+    {
+        public string Name { get; private set; }
+        public int HealthPoints { get; protected set; }
+        public int AttackPoints { get; private set; }
+        public int DefencePoints { get; protected set; }
+
+        public Character(string name, int healthPoints, int attackPoints, int defencePoints)
+        {
+            Name = name;
+            HealthPoints = healthPoints;
+            AttackPoints = attackPoints;
+            DefencePoints = defencePoints;
+        }
+
+        public int Attack()
+        {
+            Console.WriteLine(Name + " attacks...");
+            return AttackPoints;
+        }
+
+        public bool IsAlive()
+        {
+            if (HealthPoints <= 0)
+            {
+                HealthPoints = 0;
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Characters/Hero.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_Game.Characters
+{
+    public class Hero : Character, IDefender
+    {
+        public int ShieldPoints { get; private set; }
+        public ISuperPower SuperPower { get; private set; }
+        private readonly SuperPower _superPowerObject;
+
+        public Hero(string name, int healthPoints, int attackPoints, ISuperPower superPower, int defencePoints, int shieldPoints) : base(name, healthPoints, attackPoints, defencePoints)
+        {
+            ShieldPoints = shieldPoints;
+            SuperPower = superPower;
+            _superPowerObject = SuperPower.AttackWithSuperPower();
+        }
+
+        public void Defend(int attackPoints)
+        {
+            Console.WriteLine("Hero " + Name + " is defending...");
+
+            if (attackPoints > ShieldPoints)
+            {
+                if (attackPoints > DefencePoints)
+                {
+                    HealthPoints -= attackPoints - DefencePoints;
+                    DefencePoints = 0;
+                }
+                else
+                {
+                    DefencePoints -= attackPoints - ShieldPoints;
+                }
+                ShieldPoints = 0;
+            }
+            else
+            {
+                ShieldPoints -= attackPoints;
+            }
+
+            GetHeroStatus();
+        }
+
+        public void GetHeroStatus()
+        {
+            if (IsAlive())
+            {
+                Console
+                .WriteLine($"Hero {Name} [ healthPoints: {HealthPoints}, defencePoints: {DefencePoints}, attackPoints: {AttackPoints}, shieldPoints: {ShieldPoints}, superPower: {_superPowerObject.PowerName} ({_superPowerObject.AttackPoints}) ]");
+            }
+            else
+            {
+                Console.WriteLine($"Hero {Name} is dead!");
+            }
+        }
+
+        public int AttackWithSuperPower()
+        {
+            System.Console.WriteLine($"Hero attacks with superpower:{_superPowerObject.PowerName}");
+
+            return _superPowerObject.AttackPoints;
+        }
+
+        public void SetSuperPower(ISuperPower superPower)
+        {
+            SuperPower = superPower;
+        }
+    }
+}
